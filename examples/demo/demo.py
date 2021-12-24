c907004da207c90f87e6f8a9b0e69f89bab9d21e
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2021-12-24 14:03:36 +0100

    API documentation.

--- examples/demo/demo.py
+++ examples/demo/demo.py
@@ -1,32 +1,41 @@
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, FileType
 from time import sleep
+from typing import BinaryIO
 
 from simple_rpc import Interface
 
 
-def demo(device):
-    """"""
-    interface = Interface(device, wait=2)
+def demo(handle: BinaryIO, device: str) -> None:
+    """Run a simple ping loop.
+
+    :arg handle: Output file.
+    :arg device: Device name.
+    """
+    interface = Interface(device)
 
     while True:
         for i in range(256):
-            print(interface.inc(i))
+            handle.write(
+                'sent: {:3d}  received: {:3d}\n'.format(i, interface.inc(i)))
             sleep(1);
 
 
-def main():
+def main() -> None:
     """Main entry point."""
     parser = ArgumentParser(
         formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument(
         'device', metavar='DEVICE', type=str, help='device')
+    parser.add_argument(
+        '-o', dest='handle', metavar='OUTPUT', type=FileType('w'),
+        default='-', help='output file')
 
     try:
         args = parser.parse_args()
     except IOError as error:
         parser.error(error)
 
-    demo(args.device)
+    demo(args.handle, args.device)
 
 
 if __name__ == '__main__':
--- serial_mux/serial_mux.py
+++ serial_mux/serial_mux.py
@@ -2,6 +2,7 @@ from os import openpty, read, set_blocking, ttyname, write
 from serial import serial_for_url
 from time import sleep, time
 from tty import setcbreak
+from typing import BinaryIO
 
 
 _commands = {
@@ -12,11 +13,17 @@ _commands = {
 
 
 class SerialMux():
-    """"""
+    """Serial multiplexer."""
     _id = 0
 
-    def __init__(self, serial, id, log=None):
-        """"""
+    def __init__(
+            self: object, serial: object, id: int, log: BinaryIO=None
+            ) -> None:
+        """
+        :arg serial: Open serial connection.
+        :arg id: Mux id.
+        :arg log: Open writeable handle to a log file.
+        """
         self._serial = serial
         self.id = id
         self._log = log
@@ -29,39 +36,56 @@ class SerialMux():
 
         self._time = time()
 
-    def _raw_read(self):
+    def _raw_read(self: object) -> bytes:
+        """Read from master serial device.
+
+        :returns: Data.
+        """
         data = self._serial.read()
         if self._log:
             self._log.write('{:012.2f} --> {:02x} ({})\n'.format(
                 time() - self._time, ord(data), data))
         return data
 
-    def _raw_write(self, data):
+    def _raw_write(self: object, data: bytes) -> None:
+        """Write to master serial device.
+
+        :arg data: Data.
+        """
         if self._log:
             self._log.write('{:012.2f} <-- {:02x} ({})\n'.format(
                 time() - self._time, ord(data), data))
         self._serial.write(data)
 
-    def _available(self):
-        """"""
+    def _available(self: object) -> int:
+        """Get the number of bytes available for reading.
+
+        :returns: Number of bytes.
+        """
         if not SerialMux._id and self._serial.in_waiting:
             SerialMux._id = ord(self._raw_read())
         if SerialMux._id == self.id:
             return 1
         return 0
 
-    def _read(self):
-        """"""
+    def _read(self: object) -> bytes:
+        """Read incoming data.
+
+        :returns: The first byte of incoming data.
+        """
         SerialMux._id = 0
         return self._raw_read()
 
-    def _write(self, data):
-        """"""
+    def _write(self: object, data: bytes) -> None:
+        """Write outgoing data.
+
+        :arg data: Data.
+        """
         self._raw_write(bytes([self.id]))
         self._raw_write(data)
 
-    def update(self):
-        """"""
+    def update(self: object) -> None:
+        """Perform pending read and write operations."""
         if self._available():
             write(self._master, self._read())
 
@@ -72,18 +96,30 @@ class SerialMux():
         self._write(data)
 
 
-def _control(connection, cmd):
-    connection.write(bytes([0, _commands[cmd]]))
+def _control(serial: object, cmd: str) -> int:
+    """Send a control comand.
 
-    if ord(connection.read()):
+    :arg serial: Open serial connection.
+    :arg cmd: Command.
+
+    :returns: Resonse of control command.
+    """
+    serial.write(bytes([0, _commands[cmd]]))
+
+    if ord(serial.read()):
         raise IOError('Invalid control response.')
 
-    return ord(connection.read())
+    return ord(serial.read())
 
 
-def serial_mux(handle, log_handle, name):
-    """"""
-    connection = serial_for_url(name)
+def serial_mux(handle: BinaryIO, log_handle: BinaryIO, device: str) -> None:
+    """Serial multiplexer.
+
+    :arg handle: Output file.
+    :arg log_handle: Log file.
+    :arg device: Device name.
+    """
+    connection = serial_for_url(device)
     sleep(2)
 
     number_of_ports = _control(connection, 'get_ports')
